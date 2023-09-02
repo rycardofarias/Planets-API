cc19c6b9a91419bd185308dcbc21f9a6155852b4
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-09-02 13:15:55 -0300

    Created PlanetConstants

---
+++ src/test/java/common/PlanetConstants.java
@@ -0,0 +1,8 @@
+package common;
+
+import com.example.planetsAPI.entities.Planet;
+
+public class PlanetConstants {
+
+	public static final Planet PLANET = new Planet("name", "climate", "terrain");
+}
