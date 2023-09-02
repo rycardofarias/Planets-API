386ebeaa374bc9b6b8e4e0f8d64a08d478f1279a
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-09-01 22:39:13 -0300

    Project created

---
+++ src/main/java/com/example/planetsAPI/PlanetsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.planetsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetsApiApplication.class, args);
+	}
+
+}
