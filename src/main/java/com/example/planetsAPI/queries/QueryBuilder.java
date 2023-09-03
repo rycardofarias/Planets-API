6c00a184d9e47d9d4d11cebb883e446d92287c90
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-09-03 01:24:13 -0300

    Created QueryBuilder

---
+++ src/main/java/com/example/planetsAPI/queries/QueryBuilder.java
@@ -0,0 +1,16 @@
+package com.example.planetsAPI.queries;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+
+import com.example.planetsAPI.entities.Planet;
+
+public class QueryBuilder {
+
+	public static Example<Planet> makeQuery(Planet planet) {
+		ExampleMatcher exampleMatcher = ExampleMatcher.matchingAll()
+				.withIgnoreCase().withIgnoreNullValues();
+		
+		return Example.of(planet, exampleMatcher);
+	}
+}
