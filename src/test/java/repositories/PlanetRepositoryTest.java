64a84ca0f25058306deaeadb5ff74c88f0e622da
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-09-04 00:28:43 -0300

    Created PlanetRepositoryTest and test method createPlanet_WithValidData_ReturnsPlanet

---
+++ src/test/java/repositories/PlanetRepositoryTest.java
@@ -0,0 +1,39 @@
+package repositories;
+
+import static common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.example.planetsAPI.PlanetsApiApplication;
+import com.example.planetsAPI.entities.Planet;
+import com.example.planetsAPI.repositories.PlanetRepository;;
+
+@DataJpaTest
+@ContextConfiguration(classes = {PlanetsApiApplication.class})
+public class PlanetRepositoryTest {
+
+	@Autowired
+	private PlanetRepository planetRepository;
+	
+	@Autowired
+	private TestEntityManager testEntityManager;
+	
+	@Test
+	public void createPlanet_WithValidData_ReturnsPlanet() {
+		
+		Planet planet = planetRepository.save(PLANET);
+		
+		planet = testEntityManager.find(Planet.class, planet.getId());
+		
+		assertThat(planet).isNotNull();
+		assertThat(planet.getName()).isEqualTo(PLANET.getName());
+		assertThat(planet.getClimate()).isEqualTo(PLANET.getClimate());
+		assertThat(planet.getTerrain()).isEqualTo(PLANET.getTerrain());
+
+	}
+}
