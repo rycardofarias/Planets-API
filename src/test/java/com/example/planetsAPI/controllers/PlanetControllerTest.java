c2e06d25b5c9db6a6a2756818bba59dafad70e8a
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-09-06 01:58:47 -0300

    Created controller test method createPlanet_WithExistingName_ReturnsCreated in PlanetControllerTest

--- src/test/java/com/example/planetsAPI/controllers/PlanetControllerTest.java
+++ src/test/java/com/example/planetsAPI/controllers/PlanetControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.planetsAPI.controllers;
 
 import static common.PlanetConstants.PLANET;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -56,5 +58,16 @@ public class PlanetControllerTest {
 				.content(objectMapper.writeValueAsString(invalidPlanet))
 				.contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isUnprocessableEntity());
+	}
+	
+	@Test
+	public void createPlanet_WithExistingName_ReturnsCreated() throws Exception {
+	
+		when(planetService.create(any())).thenThrow(DataIntegrityViolationException.class);
+		
+		mockMvc.perform(post("/planets")
+				.content(objectMapper.writeValueAsString(PLANET))
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isConflict());
 	}
 }
